20c1db7de9f400a2cc3730a2bcd197bf190baad2
mndev2019 <muskanvision2019@gmail.com>
2025-07-03 17:11:18 +0530

    `Added loader component and implemented dynamic loading based on route changes`

---
+++ src/Component/Loader.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <>
+            <div className="overlay">
+                <div class="loader-spinner">
+                    <div class="loader-inner">
+                        <div class="loader-block"></div>
+                        <div class="loader-block"></div>
+                        <div class="loader-block"></div>
+                        <div class="loader-block"></div>
+                        <div class="loader-block"></div>
+                        <div class="loader-block"></div>
+                        <div class="loader-block"></div>
+                        <div class="loader-block"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Loader
--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
+import { useLocation } from 'react-router-dom';
+import Loader from '../Component/Loader';
 
 const Header = () => {
+    const location = useLocation();
+    const [showLoader, setShowLoader] = useState(false);
+
+    useEffect(() => {
+        // Show the loader when the location changes
+        setShowLoader(true);
+
+        // Hide the loader after a short delay (e.g., 2 seconds)
+        const timer = setTimeout(() => {
+            setShowLoader(false);
+        }, 1000);
+
+        // Cleanup the timer to avoid memory leaks
+        return () => clearTimeout(timer);
+    }, [location]);
     return (
         <>
-         <Navbar/>
+            {showLoader && <Loader />}
+            <Navbar />
         </>
     )
 }
