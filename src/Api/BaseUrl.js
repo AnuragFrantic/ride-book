f8251da07f31a3bd3fbfa8c34bc91cb7a6a64fea
AnuragFrantic <anurag@franticpro.com>
2025-11-11 18:56:23 +0530

    `Update BaseUrl and BaseImgUrl constants to use HTTPS protocol`

--- src/Api/BaseUrl.js
+++ src/Api/BaseUrl.js
@@ -2,5 +2,5 @@
 // export const BaseUrl = "http://localhost:6899/api/"
 // export const BaseImgUrl = "http://localhost:6899/"
 
-export const BaseUrl = "http://api.agraz.in/api/"
-export const BaseImgUrl = "http://api.agraz.in/"
+export const BaseUrl = "https://api.agraz.in/api/"
+export const BaseImgUrl = "https://api.agraz.in/"
