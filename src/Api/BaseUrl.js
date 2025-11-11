d03d26fb0520add372f4cfc3a3add17ad116e906
AnuragFrantic <anurag@franticpro.com>
2025-11-11 18:51:57 +0530

    `Update BaseUrl and BaseImgUrl constants in BaseUrl.js`

--- src/Api/BaseUrl.js
+++ src/Api/BaseUrl.js
@@ -1,3 +1,6 @@
-// export const BaseUrl = "https://rinsezone.com:5565/"
-export const BaseUrl = "http://localhost:6899/api/"
-export const BaseImgUrl = "http://localhost:6899/"
+
+// export const BaseUrl = "http://localhost:6899/api/"
+// export const BaseImgUrl = "http://localhost:6899/"
+
+export const BaseUrl = "http://api.agraz.in/api/"
+export const BaseImgUrl = "http://api.agraz.in/"
